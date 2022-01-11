0f73d9369b9e1a9f76bb865ba07f3dc9e8190664
romes <rodrigo.m.mesquita@gmail.com>
2022-01-11 19:08:42 +0000

    get amount of pushups (hacky)

--- app/RecordM.hs
+++ app/RecordM.hs
@@ -1,15 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 module RecordM where
 
+import GHC.Generics
+import Control.Lens hiding ((.=))
+import Data.Functor
+import Control.Monad
+
 import Data.Aeson
+import Data.Aeson.Types
+import Data.Aeson.Lens
+
 import Data.Time.Clock
 import Data.Time.Calendar
 import Data.Text as T (Text)
 import Data.Text.Encoding
-import Network.HTTP.Conduit
+import Data.ByteString as BS
+
+import Network.HTTP.Conduit as Net
 import Network.HTTP.Simple
 import Network.HTTP.Types
-import Data.ByteString as BS
+
+import qualified Database.Bloodhound
+
 
 type DefinitionName = Text
 type Token = ByteString
@@ -18,15 +30,10 @@ data Session = Session { tlsmanager :: Manager
                        , serverhost :: Host
                        , cobtoken   :: Cookie }
 
+
 class ToJSON a => Record a where
     definitionName :: a -> DefinitionName
 
-
-past :: UTCTime
-past = UTCTime (ModifiedJulianDay 56200) (secondsToDiffTime 0)
-
-future :: UTCTime
-future = UTCTime (ModifiedJulianDay 562000) (secondsToDiffTime 0)
 
 makeSession :: Host -> Token -> IO Session
 makeSession host tok = do
@@ -42,24 +49,62 @@ makeSession host tok = do
                                            , cookie_persistent       = True
                                            , cookie_host_only        = False
                                            , cookie_http_only        = False }
+    where
+    past = UTCTime (ModifiedJulianDay 56200) (secondsToDiffTime 0)
+    future = UTCTime (ModifiedJulianDay 562000) (secondsToDiffTime 0)
 
+
+cobDefaultRequest :: Session -> Request
+cobDefaultRequest session =
+    setRequestManager (tlsmanager session) $
+    Net.defaultRequest { secure    = True
+                       , port      = 443
+                       , host      = serverhost session
+                       , cookieJar = Just $ createCookieJar [cobtoken session] }
 
 integrationPOST :: Record a => Session -> a -> IO ()
 integrationPOST session record = do
-    let request = setRequestManager (tlsmanager session) $
-                  setRequestBodyJSON
-                  (object [ "type"   .= definitionName record
-                          , "values" .= record ])
-                  defaultRequest { secure = True
-                                 , method = "POST"
-                                 , host = serverhost session
-                                 , path = "/recordm/recordm/instances/integration"
-                                 , port = 443
-                                 -- , requestVersion = http20
-                                 , cookieJar = Just $ createCookieJar [cobtoken session] }
+    let request = setRequestBodyJSON
+
+                  (object
+                      [ "type"   .= definitionName record
+                      , "values" .= record ])
+
+                  (cobDefaultRequest session)
+                      { method = "POST"
+                      , path   = "/recordm/recordm/instances/integration" }
+
     response <- httpJSON request
     print $ "The status code was: " <> show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
     print $ toJSON (getResponseBody response :: Value)
 
 
+-- TODO: How to name this, see bloodhound aggregations
+
+-- definitionSearch :: Session -> DefinitionName -> IO Int
+-- definitionSearch session defname = do
+--     print $ toJSON $ mkAggregateSearch Nothing $ mkAggregations "users" $ TermsAgg $ mkTermsAggregation "user"
+
+newtype Val = Val {
+    value :: Int
+} deriving (Generic)
+instance FromJSON Val
+
+definitionSearch :: Session -> DefinitionName -> Value -> IO (Maybe Int)
+definitionSearch session defname query = do
+    print query
+    let request = setRequestBodyJSON query
+                  (cobDefaultRequest session)
+                      { method = "POST"
+                      , path   = "/recordm/recordm/definitions/search/advanced/11?size=0" }
+
+    response <- httpJSON request :: IO (Response Value)
+    print response
+    let x = getResponseBody response ^? key "aggregations" . key "sum#soma"
+    print x
+    return $ value <$> (result2Maybe =<< (fromJSON <$> x :: Maybe (Result Val)))
+
+    where
+    result2Maybe :: Result a -> Maybe a
+    result2Maybe (Error _) = Nothing
+    result2Maybe (Success x) = Just x
